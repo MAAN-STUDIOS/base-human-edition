893f37efa679e67fd3f0a15e180b621553422688
Katia Alvarez <A01781097@tec.mx>
2025-04-28 18:08:06 -0600

    UPDATE: abstract class object item

--- front-end/src/core/engine/objectitem.js
+++ front-end/src/core/engine/objectitem.js
@@ -1,28 +1,15 @@
 "use strict";
 import { GameObject } from "./gameobject";
+import { hitbox } from "../utils/hitbox.js";
 
 class ObjectItem extends GameObject{
     constructor(options = {}){
         super(options);
         this.type = options.type || "default"
+        this.hitbox = hitbox(this.position, this.width, this.height, { isLogical: true, isPhysical: false });
     }
 
     use(player){
-        if(this.type === "arma"){
-
-        }else if(this.type === "herramienta"){
-
-        }else if(this.type === "consumible"){
-
-        }else if(this.type === "modulo"){
-
-        }else if(this.type === "registro"){
-
-        }else if(this.type === "comida"){
-
-        }else if(this.type === "bateria"){
-
-        }
         return undefined;
     }
 }
