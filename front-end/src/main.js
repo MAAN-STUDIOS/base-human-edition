998c49089d9f65cacf3c7c9718ca329c48fe5149
Isabela V <150187367+IsabelaValls14@users.noreply.github.com>
2025-05-14 13:06:47 -0600

    Added main start screen

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,8 @@
 import { registerScreen, navigate } from '@utils/router.js';
 import menu from '@/screens/menu.js';
-// import testScene from '@/screens/testscene.js'; 
+
 registerScreen('menu', menu);
-registerScreen('test', testScene);
+
 
 navigate(location.pathname.slice(1) || 'menu');
 window.navigate = navigate;
