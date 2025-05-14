ada9d6135cd483614a2f90fd68a86e0b30bdecaf
Angel Montemayor Davila <A01785840@tec.mx>
2025-05-14 14:27:26 -0600

    Add game screen and update styles

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,10 +1,12 @@
 import { registerScreen, navigate } from '@utils/router.js';
 import menu from '@/screens/menu.js';
 import startgame from '@/screens/startgame.js';
+import gameScreen from "@screens/game.js";
+
 
 registerScreen('menu', menu);
 registerScreen('play', startgame);
-
+registerScreen('game', gameScreen);
 
 navigate(location.pathname.slice(1) || 'menu');
 window.navigate = navigate;
