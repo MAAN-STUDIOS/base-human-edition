a87b19ec7643b980a5b2dfc44dd09555d0260dcd
Angel Montemayor Davila <A01785840@tec.mx>
2025-05-15 13:03:05 -0600

    Update objPLayer

--- front-end/src/core/engine/objectPlayer.js
+++ front-end/src/core/engine/objectPlayer.js
@@ -1,7 +1,6 @@
-// objectplayer.js
 import { GameObject } from "./gameobject";
-import { vector } from "../utils/vector.js";
-import { hitbox } from "../utils/hitbox.js";
+import Vector from "../utils/vector.js";
+
 
 export class Player extends GameObject {
   constructor({
@@ -10,21 +9,18 @@ export class Player extends GameObject {
     height,
     controlType = "player",
     attackSlots = [null, null],
-    direction = vector(1, 0),
+    direction = new Vector(1, 0),
   } = {}) {
-    super(position, width, height);
+    super({position, width, height});
 
     this.controlType = controlType;   // "player" o "ai"
     this.attackSlots = attackSlots;   // Dos ranuras de arma
     this.activeSlot = 0;              // Índice 0 o 1
     this.direction = direction;       // Dirección de ataque/mirada
-
-    // Hitbox físico para detección de colisiones reales
-    this.hitbox = hitbox(this, 0, 0, null, null);
   }
 
   move(dir) {
-    this.position = this.position.plus(dir);
+    this.position = this.position.add(dir);
     this.direction = dir;
     this.hitbox.moveTo(this.position.x, this.position.y);
   }
