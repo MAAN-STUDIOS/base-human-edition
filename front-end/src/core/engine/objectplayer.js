ba7aab67a547c09fb3a45beaf18b3159f5315888
Mariano <a01029708@tec.mx>
2025-05-19 11:54:16 -0600

    Fixed merge issue

--- front-end/src/core/engine/objectplayer.js
+++ front-end/src/core/engine/objectplayer.js
@@ -1,8 +1,15 @@
-
 import { GameObject } from "./gameobject.js";
 import { Hitbox } from "@utils/hitbox.js";
 
-export class Player extends GameObject {
+const PLAYER_SPEED = 5;
+const MAX_HEALTH = 100;
+const WEAPON_DAMAGE = {
+    pistola: 10,
+    granada: 30
+};
+const PISTOL_RANGE = 500; // Rango máximo de la pistola
+
+class Player extends GameObject {
     constructor(options = {}) {
         super(options);
         this.health = MAX_HEALTH;
@@ -10,27 +17,56 @@ export class Player extends GameObject {
         this.hitbox = new Hitbox(this);
     }
 
-
-        this.controlType = controlType;   // "player" o "ai"
-        this.attackSlots = attackSlots;   // Dos ranuras de arma
-        this.activeSlot = 0;              // Índice 0 o 1
-        this.direction = direction;       // Dirección de ataque/mirada
-    }
-
+    // Mover al jugador en una dirección
     move(dir) {
-        this.position = this.position.add(dir);
-        this.direction = dir;
-        this.hitbox.moveTo(this.position.x, this.position.y);
-    }
-
-
-    interact(obj) {
-        if (obj && typeof obj.onInteract === "function") {
-            obj.onInteract(this);
+        const speed = 5;
+        switch (dir) {
+            case "up":
+                this.position.y -= speed;
+                break;
+            case "down":
+                this.position.y += speed;
+                break;
+            case "left":
+                this.position.x -= speed;
+                break;
+            case "right":
+                this.position.x += speed;
+                break;
         }
     }
 
+    // Interactuar con un objeto
+    interact(obj) {
+        console.log(`Interacting with ${obj}`);
+    }
 
+    // Atacar 
+    attack(target) {
+        if (this.attackSlots[0] === "pistola") {
+            this.shoot(target);
+        } else if (this.attackSlots[0] === "granada") {
+            this.throwGrenade(target);
+        }
+    }
+
+    shoot(target) {
+        // Calcular la distancia entre el jugador y el objetivo
+        const dx = target.position.x - this.position.x;
+        const dy = target.position.y - this.position.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        // Solo disparar si el objetivo está dentro del rango
+        if (distance <= PISTOL_RANGE) {
+            target.takeDamage(WEAPON_DAMAGE.pistola);
+        } else {
+            console.log("Objetivo fuera de rango");
+        }
+    }
+
+    throwGrenade(target) {
+        // Lógica para lanzar granada
+    }
 
     // Método de colisión con enemigos o estructuras
     checkCollisions(objects) {
@@ -55,14 +91,8 @@ export class Player extends GameObject {
     
         if (window.DEBUG_MODE) {
             this.hitbox.drawDebug(ctx);
-
-    attack() {
-        const weapon = this.attackSlots[this.activeSlot];
-        if (weapon && typeof weapon.fire === "function") {
-            weapon.fire(this.position, this.direction);
-
         }
     }
 }
 
-
+export { Player };
