50d7682a6b9717debc35ceaf38514a2d32e85e3d
AngelMontemayorDavila <angel.md@icloud.com>
2025-05-19 14:27:45 -0600

    Add 404 page

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,12 +1,18 @@
 import { registerScreen, navigate } from '@utils/router.js';
-import menu from '@/screens/menu.js';
-import startgame from '@/screens/startgame.js';
-import gameScreen from "@screens/game.js";
+import screenMenu from '@/screens/menu.js';
+import screenStartGame from '@/screens/startgame.js';
+import screenGame from "@screens/game.js";
+import screenFloodTest from "@/screens/floodtest.js";
+import screenPageNotFound from "@screens/404.js";
+import screenCredits from "@screens/credits.js";
 
 
-registerScreen('menu', menu);
-registerScreen('play', startgame);
-registerScreen('game', gameScreen);
+registerScreen('menu', screenMenu);
+registerScreen('play', screenStartGame);
+registerScreen('game', screenGame);
+registerScreen('flood', screenFloodTest);
+registerScreen(404, screenPageNotFound);
+registerScreen('credits', screenCredits);
+
 
 navigate(location.pathname.slice(1) || 'menu');
-window.navigate = navigate;
